470cdc8777b187e1a0d029b20c7c31f05423e9e2
Rafael Schettino <rafael_tecnologia@outlook.com>
2020-06-02 14:06:55 -0300

    Update postTemplate.js

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -20,7 +20,7 @@ export default ({ data }) => {
           <div className="container-md f4 text-left border rounded-2 bg-white p-3 p-sm-5 mt-6">
             <p className="f5"><span className="d-flex flex-items-center" style={{ color: "#0366d6" }}>
               <Octicon icon={ChevronLeft} size={16} verticalAlign='middle' ariaLabel="Home" className="mr-2" />
-              <Link to='/'>Home</Link>
+              <Link to='/'>Home!</Link>
             </span>
             </p>
             <h1 className="f00-light lh-condensed">{post.frontmatter.title}</h1>
